f76e5644ee2482b6928f78ca8f49f34412ff47ff
Robbo00 <rscrog606@west-mec.org>
2025-04-09 18:20:22 -0700

    asdfghjkl;

---
+++ tracker.js
@@ -0,0 +1,17 @@
+let game_1 = prompt("Pick the 1st game")
+let game_2 = prompt("Pick the 2nd game")
+let game_3 = prompt("Pick the 3rd game")
+let game_4 = prompt("Pick the 4th game")
+let game_5 = prompt("Pick the 5th game")
+let game_6 = prompt("Pick the 6th game")
+
+let person_1 = prompt("Pick the 1st person")
+let person_2 = prompt("Pick the 2nd person")
+let person_3 = prompt("Pick the 3rd person")
+
+let tracker = {
+    P1: person_1,
+    P2: person_2,
+    P3: person_3,
+}
+let yurr = new Map(Object.entries(dnd))
